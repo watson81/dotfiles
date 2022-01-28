c2016e2b6c9d08ffa141b0dcb01b3e3ea168f951
Patrick Watson <watson81@users.noreply.github.com>
2022-01-27 20:28:31 -0500

    add aws-get-all-instance-counts.sh

---
+++ home/bin/aws-get-all-instance-counts.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+read -rep "Re-login to all AWS accounts? [Y] " -n 1  DO_LOGIN
+[ -z "${DO_LOGIN}" ] || [ "${DO_LOGIN,,}" = "y" ] && ! aws-login-all.sh && exit 1
+
+FINAL_FILE="$(date +%FT%T%z | tr : _ ) Kabbage AWS Instance Counts.csv"
+TMP_FILE="$(mktemp)"
+
+echo "Getting instance counts" >&2
+if ! aws-for-each.sh aws-get-instance-counts.sh > "${TMP_FILE}" ; then
+    echo "Failed to get instance counts. Check ${TMP_FILE}" >&2
+elif ! json-to-csv.sh "${TMP_FILE}" > "${FINAL_FILE}" ; then
+    echo "Failed to convert JSON to CSV. Check ${TMP_FILE}" >&2
+else
+    rm "${TMP_FILE}"
+fi
