e5e429c4d3f8a72be3f57f0b188600fe21fd13cb
Patrick Watson <watson81@users.noreply.github.com>
2022-06-30 17:24:16 -0400

    aws-login-all.sh can skip prompting for accounts

--- home/bin/aws-login-all.sh
+++ home/bin/aws-login-all.sh
@@ -22,12 +22,14 @@ function promptYN {
 }
 
 promptYN "Reset AWS sessions?" && { rm -Pf ~/.aws/credentials ; touch ~/.aws/credentials ; }
+PROMPT_ALL_ROLES="y"
+promptYN "Log into all avaliable roles?" && PROMPT_ALL_ROLES="n"
 
 for arn in "${KBG_AWS_ROLES[@]}"; do
     accountid=$(echo "$arn" | sed -r 's/^([^:]*:){4}([^:]+).+/\2/')
     role=$(echo "$arn" | sed -r 's/^([^:]*:){5}role\/(.+)/\2/')
     account=$(aws-accountid-to-name.sh "${accountid}")
-    if promptYN "Log into \e[1;33m%s\e[0m as \e[1;32m%s\e[0m?" "${account}" "${role}" ; then
+    if [ "${PROMPT_ALL_ROLES,,}" = "n" ] || promptYN "Log into \e[1;33m%s\e[0m as \e[1;32m%s\e[0m?" "${account}" "${role}" ; then
         printf "Logging into \e[0;36m%s\e[0m\n" "${arn}"
         saml2aws login --skip-prompt --duo-mfa-option="Duo Push" --force --role="${arn}" --profile="${account}" --quiet "$@"
         s2a_rc="$?"
