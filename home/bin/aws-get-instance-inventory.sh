b00fc62d653a74c8f6c851c176cb1d5ef532367b
Patrick Watson <watson81@users.noreply.github.com>
2022-06-30 21:01:11 -0400

    Add basic ec2 inventory script

---
+++ home/bin/aws-get-instance-inventory.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+ACCOUNT_ALIAS=$(aws iam list-account-aliases --query AccountAliases[0] --output text)
+
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].{State:State.Name, InstanceId:InstanceId, PrivateDnsName:PrivateDnsName, Tags:Tags }' | \
+    jq 'flatten | .[] | { InstanceId: .InstanceId , InstanceName: ( .Tags[] | select(.Key == "Name" ).Value ) , AccountId: "'"${ACCOUNT_ID}"'" , AccountAlias: "'"${ACCOUNT_ALIAS}"'" , Region: "'"${AWS_DEFAULT_REGION}"'" , State: .State, PrivateDnsName: .PrivateDnsName, Tags: ( .Tags | reduce .[] as $tag (""; . + $tag.Key + "=" + $tag.Value + "|") ) }'
