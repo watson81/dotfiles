d2f7761b61ee4206f16ed2199bf416bf87503c3d
Patrick Watson <watson81@users.noreply.github.com>
2022-09-09 19:33:48 -0400

    Make AWS inventory file names generic

--- home/bin/aws-get-all-instance-counts.sh
+++ home/bin/aws-get-all-instance-counts.sh
@@ -3,7 +3,7 @@
 read -rep "Re-login to all AWS accounts? [Y] " -n 1  DO_LOGIN
 [ -z "${DO_LOGIN}" ] || [ "${DO_LOGIN,,}" = "y" ] && ! aws-login-all.sh && exit 1
 
-FINAL_FILE="$(date +%FT%T%z | tr : _ ) Kabbage AWS Instance Counts.csv"
+FINAL_FILE="$(date +%FT%T%z | tr : _ ) AWS Instance Counts.csv"
 TMP_FILE="$(mktemp)"
 
 echo "Getting instance counts" >&2
