bf3bd4980c8a53676676aedbc4055595f1188569
Patrick Watson <watson81@users.noreply.github.com>
2022-09-09 19:31:56 -0400

    Add AMI ID to instance inventories

--- home/bin/aws-get-instance-inventory.sh
+++ home/bin/aws-get-instance-inventory.sh
@@ -5,5 +5,5 @@ set -e
 ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 ACCOUNT_ALIAS=$(aws iam list-account-aliases --query AccountAliases[0] --output text)
 
-aws ec2 describe-instances --query 'Reservations[*].Instances[*].{State:State.Name, InstanceId:InstanceId, PrivateDnsName:PrivateDnsName, Tags:Tags }' | \
-    jq 'flatten | .[] | { InstanceId: .InstanceId , InstanceName: ( .Tags[] | select(.Key == "Name" ).Value ) , AccountId: "'"${ACCOUNT_ID}"'" , AccountAlias: "'"${ACCOUNT_ALIAS}"'" , Region: "'"${AWS_DEFAULT_REGION}"'" , State: .State, PrivateDnsName: .PrivateDnsName, Tags: ( .Tags | reduce .[] as $tag (""; . + $tag.Key + "=" + $tag.Value + "|") ) }'
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].{State:State.Name, InstanceId:InstanceId, PrivateDnsName:PrivateDnsName, Tags:Tags, ImageId:ImageId }' | \
+    jq 'flatten | .[] | { InstanceId: .InstanceId , InstanceName: ( .Tags[] | select(.Key == "Name" ).Value ) , AccountId: "'"${ACCOUNT_ID}"'" , AccountAlias: "'"${ACCOUNT_ALIAS}"'" , Region: "'"${AWS_DEFAULT_REGION}"'" , State: .State, PrivateDnsName: .PrivateDnsName, ImageId: .ImageId, Tags: ( .Tags | reduce .[] as $tag (""; . + $tag.Key + "=" + $tag.Value + "|") ) }'
