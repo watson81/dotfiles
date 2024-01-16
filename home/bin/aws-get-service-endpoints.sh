812dcbd2dffabab8e7dd9130ba753f850a18c98a
Patrick Watson <watson81@users.noreply.github.com>
2024-01-16 18:54:57 -0500

    Add useful error reporting

--- home/bin/aws-get-service-endpoints.sh
+++ home/bin/aws-get-service-endpoints.sh
@@ -17,7 +17,7 @@ function _getAllEndpoints() {
         while read -r REGION ; do
             j=$((j+1))
             _printStatus "${SERVICE}" "${i}" "${REGION}" "${j}"
-            aws ssm get-parameter --name "/aws/service/global-infrastructure/regions/${REGION}/services/${SERVICE}/endpoint" --query 'Parameter.Value' --output text
+            aws ssm get-parameter --name "/aws/service/global-infrastructure/regions/${REGION}/services/${SERVICE}/endpoint" --query 'Parameter.Value' --output text || echo "${SERVICE} in ${REGION} returned an error"
         done <<< "${REGIONS}"
         printf "\n" >&2
     done <<< "${SERVICES}"
@@ -27,5 +27,5 @@ function _printStatus() {
     printf "\r\e[0;34m[INFO]\e[0m Retrieving endpoints for %s (%03d/%03d). Region %02d of %02d" "${1}" "${2}" "${SERVICE_COUNT}" "${4}" "${REGION_COUNT}" >&2
 }
 
-printf "\e[0;34m[INFO]\e[0m This script may take multiple hours to run. Please ensure that your credentials do not time out."  >&2
+printf "\e[0;34m[INFO]\e[0m This script may take multiple hours to run. Please ensure that your credentials do not time out.\n"  >&2
 _getAllEndpoints | sort | uniq
